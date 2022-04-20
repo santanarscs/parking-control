d8fb9298c7d664ac65c32d00475d4d5ba250d8f5
santanarscs <raphaelstn@gmail.com>
2022-04-20 14:24:47 -0300

    feat: add intl config

---
+++ api/src/main/java/com/santanarscs/api/config/IntlConfig.java
@@ -0,0 +1,30 @@
+package com.santanarscs.api.config;
+
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Configuration
+public class IntlConfig {
+  
+
+  @Bean
+  public MessageSource messageSource() {
+    ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+    messageSource.setBasename("classpath:messages");
+    messageSource.setDefaultEncoding("ISO-8859-1");
+    messageSource.setDefaultLocale( Locale.getDefault() ); 
+    return messageSource;
+  }
+
+  @Bean
+  public LocalValidatorFactoryBean validatorFactorBean() {
+    LocalValidatorFactoryBean bean =  new LocalValidatorFactoryBean();
+    bean.setValidationMessageSource(messageSource());
+    return bean;
+  }
+}
